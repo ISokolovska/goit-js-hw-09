ca3bb1e20084717df0943f145d1e5064b115d35d
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-10-26 13:22:17 +0200

    дз 9 ч.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const buttonStart = document.querySelector(['button[data-start]']);
+const buttonStop = document.querySelector(['button[data-stop]']);
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    let randomBodyColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomBodyColor;
+  }, 1000);
+  buttonStart.setAttribute('disabled', '');
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.removeAttribute('disabled');
+});
