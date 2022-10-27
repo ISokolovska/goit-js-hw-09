250fa51305463d83f984aa74ca765f3713c46751
ISokolovska <110545422+ISokolovska@users.noreply.github.com>
2022-10-27 20:15:03 +0000

    Deploying to gh-pages from @ ISokolovska/goit-js-hw-09@b254b87408370f70342ceca9e517dfb28cb0adac 🚀

---
+++ 01-color-switcher.cdf30c8b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(["button[data-start]"]),e=document.querySelector(["button[data-stop]"]);let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.cdf30c8b.js.map
