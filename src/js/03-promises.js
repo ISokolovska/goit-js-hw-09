396a94a40855578ccc6fe254e4d4a3d6d0c11bb7
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-10-27 18:30:29 +0200

    дз 9 ч.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -41,34 +41,3 @@ function createPromise(position, delay) {
 
   return promise;
 }
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-// let timer = 500;
-// for (let i = 1; i <= 5; i++) {
-//   const isSuccess = Math.random() > 0.3;
-
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (isSuccess) {
-//         resolve('Success! Value passed to resolve function');
-//       } else {
-//         reject('Error! Error passed to reject function');
-//       }
-//     }, timer);
-//   });
-//   timer += 500;
-//   promise
-//     .then(value => {
-//       console.log(value);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
